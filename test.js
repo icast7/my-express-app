6a0a1f49c7210c4e638777fa6a66fd054299b275
icaste <ignacio.castillejos@nike.com>
2016-06-19 10:35:15 -0700

    Auth in query params

--- test.js
+++ test.js
@@ -1,27 +1,42 @@
 const app = require('express')();
 
+const bodyParser = function(req, res, next) {
+    let body ='';
+    req.on('data', chunk => {
+        body += chunk;
+    });
+    req.on('end', () => {
+        req.body = body ? JSON.parse(body) : null;
+        next();
+    });
+};
+
+function checkAuth(req, res, next) {
+    if (req.query.token && req.query.token === 'sekrit') {
+        next();
+    }
+    else {
+        res.status(401).send('Not Authorized');
+    }
+}
+
 app.use((req, res, next) => {
 	console.log(req.method, req.url);
 	next();
 });
 
-app.use((req, res, next) => {
-    console.log(req.param('test'));
-    var query = req.param('test');
-    var body = '';
-    req.on('req.query', chunk => {
-        query += chunk;
-    });
-    req.on('end', () => {
-        req.body = query ? JSON.parse(query) : null;
-        next();
-    })
+app.get('/', checkAuth, (req, res) => {
+    res.send('hello world');
 });
 
-// app.get('/', ()) {
-//
-// }
+app.get('/open', (req, res) => {
+    res.send('hello world');
+});
 
+
+app.post('/echo', checkAuth, bodyParser, (req, res, next) => {
+   res.send(req.body);
+});
 
 const port = 8082;
 app.listen(port, () => console.log('running on port', port));
